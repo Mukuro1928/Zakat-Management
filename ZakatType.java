16138dd665c7b857e0a8cadde0b917083e86dda1
Mukuro1928 <a.mukhlis123@gmail.com>
2026-01-12 01:25:03 +0800

    Implement zakatPerniagaan calculation logic
    Added calculation logic for zakatPerniagaan method.

--- ZakatType.java
+++ ZakatType.java
@@ -7,8 +7,19 @@ public class ZakatType {
         return result;
     }
     
-    public static double zakatPerniagaan(){
+    public static double zakatPerniagaan(double income,double profit, double debtGiven, double stock,double  unpaidDebt,double  tax, double outstandingOp){
         double result=0;
+        double liability, asset, totalWealth;
+        asset=profit+debtGiven+stock;
+        liability=unpaidDebt + tax + outstandingOp;
+        totalWealth= asset - liability;
+        if(totalWealth>=49300){
+            result=totalWealth*(2.5/100);
+        }
+        else {
+            result=-1;
+        }
+        
         
         return result;
     }
@@ -37,4 +48,5 @@ public class ZakatType {
         return result;
     }
 }
+
 
